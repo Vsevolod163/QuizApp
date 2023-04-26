2dd0eaa9ea2dc64dba6f0572412230f710a8b7a5
Vsevolod <vsevolod.lashin@gmail.com>
2023-04-26 13:24:04 +0300

    Model question added

--- QuizApp/Models/Question.swift
+++ QuizApp/Models/Question.swift
@@ -6,3 +6,18 @@
 //
 
 
+struct Question {
+    let flag: String
+    let answers: [String]
+    let rightAnswer: String
+    
+    static func getQuestions() -> [Question] {
+        [
+            Question(
+                flag: "Russia",
+                answers: ["Иран", "Турция", "Россия", "Китай"],
+                rightAnswer: "Россия"
+            )
+        ]
+    }
+}
